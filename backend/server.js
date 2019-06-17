ec5088ae52a46c46129ff8ae0846f86944bbc3ba
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-17 14:02:02 +1000

    added list all and get id endpoints

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = 4000;
+const todoRoutes = express.Router();
+let Todo = require("./todo.model");
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on port: " + PORT);
@@ -19,3 +22,26 @@ const mongoURI = "mongodb://localhost/todos";
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(console.log("connected to mongodb"));
+
+// using an endpoint to retrieve all the todos from the db
+todoRoutes.route("/").get((req, res) => {
+  Todo.find((err, todos) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+// getting a specific todo by id
+todoRoutes.route("/:id").get((req, res) => {
+  let id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todo);
+    }
+  });
+});
